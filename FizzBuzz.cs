92ec7545bd8d9b18d2e6534fe98f84801c22b5f9
mielonxd <47663927+mielonxd@users.noreply.github.com>
2022-03-21 14:10:39 +0100

    Add files via upload

---
+++ FizzBuzz.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace FizzBuzzWeb.Models
+{
+    public class FizzBuzz
+    {
+        [Display(Name = "Twój szczęśliwy numerek")]
+        [Required, Range(1, 1000,
+        ErrorMessage = "Oczekiwana wartość {0} z zakresu {1} i {2}.")]
+        public int Number { get; set; }
+        public string check()
+        {
+
+            if (Number % 3 == 0 && Number % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (Number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (Number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return "Liczba: " + Number +  " nie spełnia wymagań";
+            }
+
+        }
+        
+    }
+}
